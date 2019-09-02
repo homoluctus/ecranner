69e97911772c5ec322b4c464c51f911d697f6cd4
homoluctus <w.slife18sy@gmail.com>
2019-09-03 04:07:39 +0900

    update: replace command to docker sdk and boto3

--- ecranner/ecr.py
+++ ecranner/ecr.py
@@ -1,12 +1,14 @@
-import sys
 import os
+import sys
+import base64
 import boto3
 import docker
-import subprocess
 from . import log, msg
 from .exceptions import ImageMismatchedError
 
+
 LOGGER = log.get_logger()
+
 
 try:
     AWS_ACCOUNT_ID = os.environ['AWS_ACCOUNT_ID']
@@ -17,97 +19,33 @@ except KeyError:
     sys.exit(1)
 
 
-def run():
-    """ECRからDockerイメージURIを取得してPullする
+def pull_images():
+    """Pull Docker images in ECR
 
     Returns:
-        PullされたDockerイメージ名のリスト
+        pulled_image_list (list)
     """
-
-    image_name_list = get_images(IMAGE_TAG)
-    return pull_images(image_name_list)
-
-
-def get_images(tag='latest'):
-    """ECRからDockerイメージURIを取得
-
-    Returns:
-        image_uris: タグ付のイメージURIのリスト
-    """
-
-    ecr = ECRHandler()
-    image_uris = ecr.get_image_uris_filtered_by_tag(tag)
-    return image_uris
-
-
-def pull_images(image_name_list):
-    """ECRからDockerイメージをプル
-
-    Args:
-        image_name_list: 取得対象のイメージ名のリスト(空であればECRにある全てのイメージを取得)
-
-    Returns:
-        pulled_image_list: 正常にプルしたDockerイメージ名が格納されたリスト
-        None: ログイン失敗
-    """
-
-    try:
-        ECRHandler.login(AWS_DEFAULT_REGION)
-
-    except subprocess.CalledProcessError as err:
-        LOGGER.error(err.cmd)
-        LOGGER.error(err.stderr)
-        return None
-
-    except Exception:
-        LOGGER.exception(msg.ERROR)
-        return None
-
-    else:
-        LOGGER.info(msg.LOGIN_SUCCESS)
 
     pulled_image_list = []
 
-    for image_name in image_name_list:
-        try:
-            client = ECRHandler(image_name)
-            client.pull(image_name)
-
-        except Exception as err:
-            LOGGER.error(err, exc_info=True)
-
-        else:
-            LOGGER.info(f'Pulled {image_name}')
-            pulled_image_list.append(image_name)
-
-        finally:
-            # close docker session
-            client.close()
-
-    return pulled_image_list
-
-
-def execute_cmd(cmd):
-    """コマンドを実行
-
-    Args:
-        cmd: コマンドのリスト ex)['ls', '-l']
-
-    Returns:
-        proc: subprocess.CompletedProcess(コマンドが完了した場合)
-    """
-
     try:
-        proc = subprocess.run(
-            cmd, capture_output=True, check=True,
-            timeout=300, encoding='utf-8')
-
-        LOGGER.debug(proc.stdout)
-
-        return proc
+        with ECRHandler() as ecr:
+            auth_data = ecr.authorize()
+            ecr.login(**auth_data)
+            image_list = ecr.get_image_uris_filtered_by_tag(IMAGE_TAG)
+            for image_name in image_list:
+                ecr.pull(
+                    image_name,
+                    auth_data['username'],
+                    auth_data['password']
+                )
+                LOGGER.info(f'Pulled {image_name}')
+                pulled_image_list.append(image_name)
 
     except Exception as err:
-        raise err
+        LOGGER.error(err, exc_info=True)
+
+    return pulled_image_list
 
 
 class ECRHandler:
@@ -120,11 +58,19 @@ class ECRHandler:
             timeout=60,
         )
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        self.docker_client.close()
+
     def pull(self, image_name, username=None, password=None):
         """Pull Docker image
 
         Args:
-            image_name: Docker image name
+            image_name (str): Docker image name (includes tag)
+            username (str)
+            password (str)
 
         Returns:
             image_name (str): docker image name
@@ -139,13 +85,18 @@ class ECRHandler:
                 docker.errors.APIError):
             pass
 
+        if username and password:
+            auth_config = {
+                'username': username,
+                'password': password
+            }
+        else:
+            auth_config = None
+
         try:
             image = self.docker_client.images.pull(
                 image_name,
-                auth_config={
-                    'username': username,
-                    'password': password
-                }
+                auth_config=auth_config
             )
 
             pulled_image_name = image.tags[0]
@@ -153,7 +104,7 @@ class ECRHandler:
             if pulled_image_name != image_name:
                 raise ImageMismatchedError(f'''
                     Pulled image: {pulled_image_name}
-                    Expected image: {image_name}
+                    But expected: {image_name}
                 ''')
 
             return pulled_image_name
@@ -162,22 +113,72 @@ class ECRHandler:
                 ImageMismatchedError) as err:
             raise err
 
-    @classmethod
-    def login(cls, region):
-        """Login to ECR
+    def __decode_token(self, token):
+        """Decode ECR access token
 
         Args:
-            region (str)
+            token (str): ECR access token (format: base64)
+
+        Raises:
+            TypeError: raises when token is not bytes object
         """
 
-        ecr_login_cmd = [
-            'aws', 'ecr', 'get-login',
-            '--region', region, '--no-include-email'
-        ]
-        result = execute_cmd(ecr_login_cmd)
-        stdout = result.stdout.rstrip('\n')
-        docker_login_cmd = stdout.split(' ')
-        return execute_cmd(docker_login_cmd)
+        if not isinstance(token, str):
+            raise TypeError(
+                f'Expected bytes object but current object is {type(token)}')
+
+        bytes_token = base64.b64decode(token)
+        return bytes_token.decode('utf-8')
+
+    def authorize(self):
+        """Get ECR authorization access token
+
+        Returns:
+            authorization_data (dict): includes username, password and registry
+
+        Raises:
+            Exception
+        """
+
+        try:
+            res = self.ecr_client.get_authorization_token(
+                registryIds=[self.params['registryId']]
+            )
+
+            access_token = self.__decode_token(
+                res['authorizationData'][0]['authorizationToken'])
+            registry_url = res['authorizationData'][0]['proxyEndpoint']
+            username, password = access_token.split(':')
+
+            authorization_data = {
+                'username': username,
+                'password': password,
+                'registry': registry_url
+            }
+
+            return authorization_data
+
+        except Exception as err:
+            raise err
+
+    def login(self, username, password, registry):
+        """Login to ECR registry
+
+        Raises:
+            Exception
+        """
+
+        try:
+            res = self.docker_client.login(
+                username,
+                password=password,
+                registry=registry,
+                reauth=True
+            )
+            LOGGER.debug(res)
+
+        except Exception as err:
+            raise err
 
     def __get_repositories_recursively(self, params, repositories):
         """ECRから全リポジトリを再帰的に取得(self.get_repositoriesで使用する)
@@ -208,14 +209,11 @@ class ECRHandler:
             repositories:
                 ECR.Client.describe_repositoriesの返り値に含まれるrepositoriesリスト
         """
-
         if not params:
             params = self.params.copy()
 
         repositories = []
-
         self.__get_repositories_recursively(params, repositories)
-
         return repositories
 
     def get_image_tags(self, params={}):
@@ -227,7 +225,6 @@ class ECRHandler:
         Returns:
             image_tags: ECR.Client.describe_imagesの返り値のimageTagsを格納するリスト
         """
-
         if not params:
             params = self.params
 
@@ -239,7 +236,7 @@ class ECRHandler:
 
         return image_tags
 
-    def exist_tag(self, tag_list, target_tag):
+    def tag_exists(self, tag_list, target_tag):
         """Dockerイメージのタグに特定のタグが存在するかチェック
 
         Args:
@@ -247,9 +244,8 @@ class ECRHandler:
             target_tag: 対象のイメージタグ
 
         Returns:
-            boolean: 引数のtarget_tagがtag_listに存在すればTrue
+            boolean
         """
-
         if target_tag in tag_list:
             return True
         else:
@@ -290,7 +286,7 @@ class ECRHandler:
 
             tag_list = self.get_image_tags(params)
 
-            if self.exist_tag(tag_list, tag):
+            if self.tag_exists(tag_list, tag):
                 image_uri = '{}:{}'.format(repository['repositoryUri'], tag)
                 image_uris_with_tag.append(image_uri)
 
