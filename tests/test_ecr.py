e529d518003dd83853c933547d4f09699bff0cf8
homoluctus <w.slife18sy@gmail.com>
2019-09-22 15:34:35 +0900

    REMOVE - delete no need code

--- tests/test_ecr.py
+++ tests/test_ecr.py
@@ -8,56 +8,28 @@ from docker.errors import APIError as DockerAPIError
 
 
 class TestECRHandler:
-    def test_pull(self):
-        # condition: 'alpine:latest' image must not exist
-        image_name = 'alpine:latest'
-        client = ECRHandler()
-        result = client.pull(image_name)
+    def test_extract_account_id(self):
+        expected_result = '01234567'
+        registry_url = 'https://01234567.dkr.ecr.us-west-2.amazonaws.com'
+        result = ECRHandler.extract_account_id(registry_url)
+        assert result == expected_result
 
-        assert result == image_name
+#     def test_pull(self):
+#         # condition: 'alpine:latest' image must not exist
+#         image_name = 'alpine:latest'
+#         client = ECRHandler()
+#         result = client.pull(image_name)
 
-        client.remove(image_name)
-        client.docker_client.close()
+#         assert result == image_name
 
-    def test_pull_nonexistant_image(self):
-        image_name = 'nonexistant_image'
-        client = ECRHandler()
+#         client.remove(image_name)
+#         client.docker_client.close()
 
-        with pytest.raises(DockerAPIError):
-            client.pull(image_name)
+#     def test_pull_nonexistant_image(self):
+#         image_name = 'nonexistant_image'
+#         client = ECRHandler()
 
-        client.docker_client.close()
+#         with pytest.raises(DockerAPIError):
+#             client.pull(image_name)
 
-
-def test_set_credentials_as_env():
-    creds = {
-        'aws_access_key_id': 'test',
-        'aws_secret_access_key': 'test',
-        'aws_default_region': 'heaven',
-    }
-    result = set_credentials_as_env(creds, True)
-
-    assert result is True
-    assert os.environ['AWS_ACCESS_KEY_ID'] == 'test'
-    assert os.environ['AWS_SECRET_ACCESS_KEY'] == 'test'
-    assert os.environ['AWS_DEFAULT_REGION'] == 'heaven'
-
-
-def test_extract_credentials():
-    tmp = {
-        'aws_access_key_id': 'test',
-        'aws_secret_access_key': 'test',
-        'aws_default_region': 'heaven',
-    }
-    params = {
-        'not': 'credential',
-        'hello': 'world'
-    }
-    params.update(tmp)
-
-    expected_result = {
-        key.upper(): tmp[key] for key in tmp.keys()
-    }
-
-    result = extract_credentials(params)
-    assert result == expected_result
+#         client.docker_client.close()
