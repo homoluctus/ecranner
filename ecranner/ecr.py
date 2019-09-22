64f0ff474a55408389b5ae5adfe3c195d3ca18eb
homoluctus <w.slife18sy@gmail.com>
2019-09-22 15:34:23 +0900

    UPDATE - add create_ecr_client() and remove no need code

--- ecranner/ecr.py
+++ ecranner/ecr.py
@@ -1,11 +1,13 @@
+import os
+import re
 import base64
 import boto3
-import docker
+
 from .log import get_logger
 from .docker import DockerHandler
 from .exceptions import (
-    ImageMismatchedError, LoginRegistryError,
-    DecodeAuthorizationTokenError,
+    DecodeAuthorizationTokenError, AuthorizationError,
+    ExtractAccountIDError, ConfigurationError
 )
 from .config import EnvFileLoader
 
@@ -13,70 +15,86 @@ logger = get_logger()
 
 
 class ECRHandler(DockerHandler):
-    def __init__(self):
-        super().__init__()
-        self.ecr_client = boto3.client('ecr')
-        self.params = {'registryId': AWS_ACCOUNT_ID}
+    """Manipulate AWS ECR"""
 
-    def pull(self, image_name, username=None, password=None):
-        """Pull Docker image
-
+    def __init__(self,
+                 aws_access_key_id=None,
+                 aws_secret_access_key=None,
+                 region=None,
+                 profile=None):
+        """
         Args:
-            image_name (str): Docker image name (includes tag)
-            username (str)
-            password (str)
-
-        Returns:
-            image_name (str): Docker image name
+            aws_access_key_id (str)
+            aws_secret_access_key (str)
+            region (str)
+            profile (str)
         """
 
-        try:
-            # pre check if specified docker image is already pulled
-            self.docker_client.images.get(image_name)
-            logger.info(f'{image_name} already exists')
-            return image_name
-
-        except (docker.errors.ImageNotFound,
-                docker.errors.APIError):
-            pass
-
-        if username and password:
-            auth_config = {
-                'username': username,
-                'password': password
-            }
-        else:
-            auth_config = None
+        super().__init__()
 
         try:
-            image = self.docker_client.images.pull(
-                image_name,
-                auth_config=auth_config
+            self.ecr_client = self.create_ecr_client(
+                aws_access_key_id=None,
+                aws_secret_access_key=None,
+                region=None,
+                profile=None
             )
-            logger.info(f'Pulled {image_name}')
-
-            pulled_image_name = image.tags[0]
-
-            if pulled_image_name != image_name:
-                raise ImageMismatchedError(f'''
-                    Pulled image: {pulled_image_name}
-                    But expected: {image_name}
-                ''')
-
-            return pulled_image_name
-
-        except (docker.errors.APIError,
-                ImageMismatchedError) as err:
+        except Exception as err:
+            self.close()
             raise err
 
-    def __decode_token(self, token):
+    def create_ecr_client(self,
+                          aws_access_key_id=None,
+                          aws_secret_access_key=None,
+                          region=None,
+                          profile=None):
+        """Create ECR client
+
+        Args:
+            aws_access_key_id (str)
+            aws_secret_access_key (str)
+            region (str)
+            profile (str)
+
+        Returns:
+            ECR client
+
+        Raises:
+            ConfigurationError
+        """
+
+        aws_access_key_id = aws_access_key_id or os.getnev('AWS_ACCESS_KEY_ID')
+        aws_secret_access_key = aws_secret_access_key \
+            or os.getenv('AWS_SECRET_ACCESS_KEY')
+        region = region or os.getenv('AWS_DEFAULT_REGION')
+
+        if profile:
+            session = boto3.Session(profile_name=profile)
+        elif aws_access_key_id \
+                and aws_secret_access_key \
+                and region:
+            session = boto3.Session(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                region_name=region
+            )
+        else:
+            raise ConfigurationError('''
+                Missing AWS credentials.
+                Please configure AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION
+            ''')
+
+        return session.resource('ecr')
+
+    def _decode_token(self, token):
         """Decode AWS ECR access token
 
         Args:
-            token (str): AWS ECR access token (format: base64)
+            token(str): AWS ECR access token(format: base64)
 
         Returns:
-            access_token (tuple): Includes username, password
+            access_token(tuple): Includes username, password
 
         Raises:
             TypeError: raises when token is not bytes object
@@ -97,250 +115,89 @@ class ECRHandler(DockerHandler):
         access_token = tuple(decoded_token.split(':'))
         return access_token
 
-    def authorize(self):
+    def authorize(self, account_id):
         """Get AWS ECR authorization access token
 
+        Args:
+            account_id(str)
+
         Returns:
-            authorization_data (dict): includes username, password and registry
+            authorization_data(dict): includes username,
+                password and registry URI
 
         Raises:
-            Exception
+            TypeError
+            AuthorizationError
+            DecodeAuthorizationTokenError
         """
+
+        if not isinstance(account_id, str):
+            raise TypeError(
+                f'Expected type is str, but the argument is {type(account_id)}'
+            )
 
         try:
             res = self.ecr_client.get_authorization_token(
-                registryIds=[self.params['registryId']]
+                registryIds=[account_id]
             )
 
-            username, password = self.__decode_token(
-                res['authorizationData'][0]['authorizationToken'])
-            registry_url = res['authorizationData'][0]['proxyEndpoint']
+        except Exception as err:
+            raise AuthorizationError(
+                f'Failed to get authorization token: {err}'
+            )
 
-            authorization_data = {
+        logger.info('Got AWS ECR authorization token')
+
+        authorization_data = {}
+        for auth_data in res['authorizationData']:
+            username, password = self._decode_token(
+                auth_data['authorizationToken'])
+            registry_url = auth_data['proxyEndpoint']
+
+            account_id = self.extract_account_id(registry_url)
+
+            if not account_id:
+                raise ExtractAccountIDError(
+                    'Faild to extract AWS account id from registry URL'
+                )
+
+            authorization_data[account_id] = {
                 'username': username,
                 'password': password,
                 'registry': registry_url
             }
 
-            return authorization_data
+        return authorization_data
 
-        except Exception as err:
-            raise err
-
-    def login(self, username, password, registry):
-        """Login to AWS ECR registry
-
-        Raises:
-            LoginRegistryError
-        """
-
-        try:
-            res = self.docker_client.login(
-                username,
-                password=password,
-                registry=registry,
-                reauth=True
-            )
-
-        except Exception as err:
-            raise LoginRegistryError(f'Failed to Login to ECR: {err}')
-
-        else:
-            logger.debug(res)
-
-    def __get_repositories_recursively(self, params, repositories):
-        """Recursively get repositories from AWS ECR
-        Used self.get_repositories
+    @classmethod
+    def extract_account_id(cls, registry_url):
+        """Extract AWS account id from registry_url
 
         Args:
-            params (dict): the parameter of ECR.Client.describe_repositories
-            repositories (list): list to store gotten repositories
-        """
-
-        response = self.ecr_client.describe_repositories(**params)
-        repositories += response['repositories']
-
-        try:
-            if response['nextToken']:
-                params['nextToken'] = response['nextToken']
-                self.__get_repositories_recursively(params, repositories)
-        except KeyError:
-            pass
-
-    def get_repositories(self, params={}):
-        """Fetch all repository from AWS ECR
-
-        Args:
-            params (dict):
-                parameter of ECR.Client.describe_repositories()
+            registry_url(str)
 
         Returns:
-            repositories (list):
-                list includes the return value of
-                ECR.Client.describe_repositories()
+            account_id(str)
+            False
         """
 
-        if not params:
-            params = self.params.copy()
+        matched_string = re.search(r'(?<=^https:\/\/)\d+', registry_url)
 
-        repositories = []
-        self.__get_repositories_recursively(params, repositories)
-        return repositories
-
-    def get_image_tags(self, params={}):
-        """Get image tag
-
-        Args:
-            params (dict): parameter of ECR.Client.describe_images()
-
-        Returns:
-            image_tags (list):
-                store the return value 'imageTags' of
-                ECR.Client.describe_images()
-        """
-
-        if not params:
-            params = self.params.copy()
-
-        image_tags = []
-        response = self.ecr_client.describe_images(**params)
-
-        for image in response['imageDetails']:
-            image_tags += image['imageTags']
-
-        return image_tags
-
-    def tag_exists(self, tag_list, target_tag):
-        """Check if docker image includes target tag
-
-        Args:
-            tag_list (list):
-                Docker image tag list that is the return value
-                of ECR.Client.describe_images()
-            target_tag (str): target docker image tag
-
-        Returns:
-            boolean
-        """
-        if target_tag in tag_list:
-            return True
-        else:
+        if matched_string is None:
             return False
 
-    def get_image_uris(self):
-        """Get docker image URI from AWS ECR
-
-        Returns:
-            image_uris (list)
-        """
-
-        repositories = self.get_repositories()
-
-        image_uris = [
-            repository['repositoryUri'] for repository in repositories
-        ]
-
-        return image_uris
-
-    def get_image_uris_filtered_by_tag(self, tag):
-        """Get docker image URI from AWS ECR and
-        filter by docker image tag
-
-        Args:
-            tag (str): docker image tag
-
-        Returns:
-            image_uris_with_tag (list): docker image URI list
-        """
-
-        repositories = self.get_repositories()
-        params = self.params.copy()
-        image_uris_with_tag = []
-
-        for repository in repositories:
-            params['repositoryName'] = repository['repositoryName']
-            params['filter'] = {'tagStatus': 'TAGGED'}
-
-            tag_list = self.get_image_tags(params)
-
-            if self.tag_exists(tag_list, tag):
-                image_uri = '{}:{}'.format(repository['repositoryUri'], tag)
-                image_uris_with_tag.append(image_uri)
-
-        return image_uris_with_tag
+        account_id = matched_string.group(0)
+        return account_id
 
 
 def pull(config):
     """Pull Docker images in AWS ECR
 
     Args:
-        config (dict): includes aws credentials and image names want to pull
+        config(dict): includes aws credentials and image names want to pull
 
     Returns:
-        pulled_image_list (list):
-            Returns empty list if failed to pull docker image
-            or target image does not exist
+        pulled_image_list(list): Returns empty list if failed to
+            pull docker image or target image does not exist
     """
-
-    pulled_image_list = []
-
-    try:
-        with ECRHandler() as ecr:
-            auth_data = ecr.authorize()
-            ecr.login(**auth_data)
-            logger.info(f'Succeded login to ECR')
-            image_list = ecr.get_image_uris_filtered_by_tag(IMAGE_TAG)
-            for image_name in image_list:
-                ecr.pull(
-                    image_name,
-                    auth_data['username'],
-                    auth_data['password']
-                )
-                pulled_image_list.append(image_name)
-
-    except Exception as err:
-        logger.error(err, exc_info=True)
-
-    return pulled_image_list
-
-
-def set_credentials_as_env(config, override=False):
-    """Set aws credentials as system environment variable
-
-    Args:
-        config (dict)
-        override (boolean)
-
-    Returns:
-        boolean
-
-    Raises:
-        TypeError
-    """
-
-    credentials = extract_credentials(config)
-    return EnvFileLoader.set_env_from_dict(credentials, override)
-
-
-def extract_credentials(config):
-    """Extract aws credentials from config
-
-    Args:
-        config (dict)
-
-    Returns:
-        aws_credentials (dict)
-    """
-
-    params = [
-        'aws_access_key_id',
-        'aws_secret_access_key',
-        'aws_default_region'
-    ]
-
-    aws_credentials = {
-        str(key).upper(): str(config[key])
-        for key in config.keys() if key in params
-    }
-
-    return aws_credentials
+    pass
