ff7b917a62e839602e81c819491759419c5bbc98
homoluctus <w.slife18sy@gmail.com>
2019-09-22 15:53:29 +0900

    REMOVE - delete no need code

--- tests/test_ecr.py
+++ tests/test_ecr.py
@@ -8,28 +8,22 @@ from docker.errors import APIError as DockerAPIError
 
 
 class TestECRHandler:
-    def test_extract_account_id(self):
-        expected_result = '01234567'
-        registry_url = 'https://01234567.dkr.ecr.us-west-2.amazonaws.com'
-        result = ECRHandler.extract_account_id(registry_url)
-        assert result == expected_result
+    # def test_pull(self):
+    #     # condition: 'alpine:latest' image must not exist
+    #     image_name = 'alpine:latest'
+    #     client = ECRHandler()
+    #     result = client.pull(image_name)
 
-#     def test_pull(self):
-#         # condition: 'alpine:latest' image must not exist
-#         image_name = 'alpine:latest'
-#         client = ECRHandler()
-#         result = client.pull(image_name)
+    #     assert result == image_name
 
-#         assert result == image_name
+    #     client.remove(image_name)
+    #     client.docker_client.close()
 
-#         client.remove(image_name)
-#         client.docker_client.close()
+    # def test_pull_nonexistant_image(self):
+    #     image_name = 'nonexistant_image'
+    #     client = ECRHandler()
 
-#     def test_pull_nonexistant_image(self):
-#         image_name = 'nonexistant_image'
-#         client = ECRHandler()
+    #     with pytest.raises(DockerAPIError):
+    #         client.pull(image_name)
 
-#         with pytest.raises(DockerAPIError):
-#             client.pull(image_name)
-
-#         client.docker_client.close()
+    #     client.docker_client.close()
