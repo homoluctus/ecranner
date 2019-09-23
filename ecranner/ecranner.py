807fa8988a3cc6133848e6f787c21e221d958bab
homoluctus <w.slife18sy@gmail.com>
2019-09-23 11:02:32 +0900

    UPDATE - convert from LOGGER to logger

--- ecranner/ecranner.py
+++ ecranner/ecranner.py
@@ -4,7 +4,7 @@ from .config import load_dot_env, load_yaml
 from .log import get_logger
 
 
-LOGGER = get_logger()
+logger = get_logger()
 
 
 def run(kwargs):
@@ -21,13 +21,13 @@ def run(kwargs):
         image_list = ecr.pull(**aws_config)
 
     if not image_list:
-        LOGGER.info('There are no Docker images to scan')
-        LOGGER.info('TERMINATE')
+        logger.info('There are no Docker images to scan')
+        logger.info('TERMINATE')
         return
 
     payloads = []
 
-    LOGGER.info('Scanning...')
+    logger.info('Scanning...')
 
     for image in image_list:
         results = trivy.run(image)
@@ -37,26 +37,26 @@ def run(kwargs):
 
         payloads.append(slack.generate_payload(results, image))
 
-    LOGGER.info('Finised Scan')
+    logger.info('Finised Scan')
 
     if kwargs['rm']:
         remove_images(image_list)
 
     if not kwargs['slack']:
-        LOGGER.info('TERMINATE')
+        logger.info('TERMINATE')
         return
 
-    LOGGER.info('Posting to Slack...')
+    logger.info('Posting to Slack...')
     result = slack.post(payloads)
-    LOGGER.info('Posted result to Slack')
+    logger.info('Posted result to Slack')
 
     if isinstance(result, list):
         failure_num = utils.exception_exists(result)
         suffix = 'scan result messages'
-        LOGGER.info(f'''
+        logger.info(f'''
             SUCCESS: {len(result) - failure_num} {suffix}
             FAILURE: {failure_num} {suffix}
         ''')
 
-    LOGGER.info('TERMINATE')
+    logger.info('TERMINATE')
     return
