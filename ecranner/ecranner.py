faeb2a29a4b7aaf4cbe994c2d3182ba1230e1162
homoluctus <w.slife18sy@gmail.com>
2019-09-23 10:35:13 +0900

    UPDATE - add load configs

--- ecranner/ecranner.py
+++ ecranner/ecranner.py
@@ -1,21 +1,23 @@
-from . import trivy, ecr, slack, utils
+from . import trivy, slack, utils, ecr
+from .config import load_dot_env, load_yaml
 from .log import get_logger
 
 
 LOGGER = get_logger()
 
 
-def run(aws_config, trivy_options):
+def run(kwargs):
     """Execute scan and post scan result to slack
 
     Args:
-        aws_config (dict)
-        trivy_options (dict)
+        kwargs (dict)
     """
 
-    LOGGER.info('START ECRanner')
+    load_dot_env(kwargs['env-file'])
+    config = load_yaml(kwargs['file'])
 
-    image_list = ecr.pull(**aws_config)
+    for aws_config in config['aws'].values():
+        image_list = ecr.pull(**aws_config)
 
     if not image_list:
         LOGGER.info('There are no Docker images to scan')
@@ -35,10 +37,13 @@ def run(aws_config, trivy_options):
         payloads.append(slack.generate_payload(results, image))
 
     LOGGER.info('Finised Scan')
+
+    if not kwargs['slack']:
+        LOGGER.info('TERMINATE')
+        return
+
     LOGGER.info('Posting to Slack...')
-
     result = slack.post(payloads)
-
     LOGGER.info('Posted result to Slack')
 
     if isinstance(result, list):
