ee53af81ba669aa743125bde29c9beaf4ad9d49f
homoluctus <w.slife18sy@gmail.com>
2019-09-24 14:09:52 +0900

    UPDATE - remove pulled images if remove_flag is True and raise exceptions

--- ecranner/ecranner.py
+++ ecranner/ecranner.py
@@ -18,39 +18,18 @@ def run(kwargs):
 
     load_dot_env(kwargs['env_file'])
     config = load_yaml(kwargs['file'])
+    remove_flag = kwargs['rm']
 
-    pulled_image_list = []
-    for aws_config in config['aws'].values():
-        tmp = ecr.pull(**aws_config)
-
-        if isinstance(tmp, str):
-            pulled_image_list.append(tmp)
-        elif isinstance(tmp, list):
-            pulled_image_list.extend(tmp)
+    pulled_image_list = pull_images(config['aws'], remove_flag)
 
     if not pulled_image_list:
         logger.info('There are no Docker images to scan')
         logger.info('TERMINATE')
         return
 
-    payloads = []
-
     logger.info('Scanning...')
-
-    for image in pulled_image_list:
-        results = trivy.run(image)
-
-        if results is None:
-            continue
-
-        pprint(results, indent=2)
-        payloads.append(slack.generate_payload(results, image))
-
+    payloads = scan_images(pulled_image_list, remove_flag)
     logger.info('Finised Scan')
-
-    if kwargs['rm']:
-        with DockerImageHandler() as client:
-            client.remove_images(pulled_image_list)
 
     if not kwargs['slack']:
         logger.info('TERMINATE')
@@ -68,3 +47,91 @@ def run(kwargs):
 
     logger.info('TERMINATE')
     return
+
+
+def pull_images(aws_provider, remove_flag=False):
+    """Pull images from AWS ECR
+
+    Args:
+        aws_provider (dict)
+        remove_flag (boolean)
+
+    Returns:
+        pulled_image_list (list)
+    """
+
+    pulled_image_list = []
+
+    try:
+        for aws_config in aws_provider.values():
+            tmp = ecr.pull(**aws_config)
+
+            if isinstance(tmp, str):
+                pulled_image_list.append(tmp)
+            elif isinstance(tmp, list):
+                pulled_image_list.extend(tmp)
+
+    except Exception as err:
+        remove_images(pulled_image_list, remove_flag)
+        raise err
+
+    else:
+        return pulled_image_list
+
+
+def remove_images(target_image_list, remove_flag=False):
+    """Remove pulled docker images
+
+    Args:
+        target_image_list (list)
+        remove_flag (boolean)
+
+    Returns:
+        boolean
+    """
+
+    if remove_flag:
+        with DockerImageHandler() as client:
+            result = client.remove_images(target_image_list)
+
+        if isinstance(result, list):
+            logger.warning(f'Failed to remove all images: {result}')
+            return False
+
+        elif result:
+            logger.info('Remove all target images')
+
+    return True
+
+
+def scan_images(target_image_list, remove_flag=False):
+    """Scan target images
+
+    Args:
+        target_image_list (list)
+        remove_flag (boolean)
+
+    Returns:
+        True: if all target image is scanned
+    """
+
+    payloads = []
+
+    try:
+        for image in target_image_list:
+            results = trivy.run(image)
+
+            if results is None:
+                continue
+
+            pprint(results, indent=2)
+            payloads.append(slack.generate_payload(results, image))
+
+    except Exception as err:
+        raise err
+
+    else:
+        return payloads
+
+    finally:
+        remove_images(target_image_list, remove_flag)
